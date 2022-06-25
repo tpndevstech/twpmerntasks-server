2df582abe3973f51a0a4a5eb5a99924a69596719
Tapan Anand <70485285+tpndevstech@users.noreply.github.com>
2022-06-25 22:01:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/posts", postRoutes);
+
+const PORT = process.env.PORT || 5000;
+//IMPORTANT: Use "process.env.<something>" while using dotenv and .env files
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.connect(process.env.CONNECTION_URL).then(() => {
+  console.log("...");
+});
